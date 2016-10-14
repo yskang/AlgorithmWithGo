d92cb7163c1656feae27e124844fed9438b8d6ed
Kang, Yongsung <yongsung.kang@gmail.com>
2016-10-14 09:18:44 +0900

    1003, fibonacci recursion count

---
+++ fibonacciRecursionCount.go
@@ -0,0 +1,32 @@
+// https://www.acmicpc.net/problem/1003
+
+package main
+
+import "fmt"
+
+var count_0, count_1 int
+
+func main() {
+	var n, nn int
+	fmt.Scanf("%d/n", &n)
+
+	for i := 0 ; i < n ; i++ {
+		fmt.Scanf("%d/n", &nn)
+		fibonacci(nn)
+		fmt.Println(count_0, count_1)
+		count_0, count_1 = 0, 0
+	}
+
+}
+
+func fibonacci(n int) int {
+	if n == 0 {
+		count_0 += 1
+		return 0;
+	} else if n == 1 {
+		count_1 += 1
+		return 1;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2)
+	}
+}
