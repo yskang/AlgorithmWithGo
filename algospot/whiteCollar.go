d8bde58a8473ec948b605199095f98c3c6109821
Yongsung Kang <yongsung.kang@gmail.com>
2016-10-17 22:31:53 +0900

    pass!! I missed case that there is a isolated city. refactoring to reduce exe time

--- algospot/whiteCollar.go
+++ algospot/whiteCollar.go
@@ -1,10 +1,11 @@
+// https://algospot.com/judge/problem/read/WHITECOLLAR
+
 package main
 
 import (
 	"fmt"
 	"strconv"
 	"strings"
-	"sort"
 	"math"
 	"bufio"
 	"os"
@@ -12,15 +13,17 @@ import (
 
 func main() {
 	var numTest int
-	fmt.Scanf("%d\n", &numTest)
-
 	scanner := bufio.NewScanner(os.Stdin)
+	var in string
+	for scanner.Scan() {
+		in = scanner.Text()
+		break
+	}
+	numTest, _ = strconv.Atoi(in)
 
 	for t := 0 ; t < numTest ; t++ {
-		graph := InitGraph()
 		var inLine string
 		var N, M int
-		//fmt.Scanf("%d %d\n", &N, &M)
 
 		for scanner.Scan() {
 			inLine = scanner.Text()
@@ -30,9 +33,10 @@ func main() {
 		N, _ = strconv.Atoi(ins[0])
 		M, _ = strconv.Atoi(ins[1])
 
+		graph := InitGraph(N)
+
 		for m := 0 ; m < M ; m++ {
 			var cA, cB int
-			//fmt.Scanf("%d %d\n", &cA, &cB)
 			for scanner.Scan() {
 				inLine = scanner.Text()
 				break
@@ -41,85 +45,64 @@ func main() {
 			cA, _ = strconv.Atoi(ins[0])
 			cB, _ = strconv.Atoi(ins[1])
 
-			graph.AddNode(cA)
-			graph.AddNode(cB)
-			graph.AddEdge(cA, cB, 10)
+			graph.AddEdge(cA, cB, 1)
 		}
-		path, _ := Dijsktra(graph, 1)
-		//past := make([]int, 0)
-		//multiPath := make([][]int, 0)
-		//GetMultiPath(path, 1, N, past, &multiPath)
-		cities := make([]int, 0)
+		path, _ := dijsktra(graph, 1)
+		cities := make([]bool, graph.GetNumOfNodes()+1)
 		DFS(&path, N, &cities)
 		fmt.Println(makeString(cities))
 	}
 }
-func makeString(cities []int) string {
+func makeString(cities []bool) string {
 	str := make([]string, 0)
-	sort.Ints(cities)
-	for _, city := range cities {
-		str = append(str, strconv.Itoa(city))
+	for city, visit := range cities {
+		if visit {
+			str = append(str, strconv.Itoa(city))
+		}
 	}
 	return strings.Join(str, " ")
 }
-func getCities(paths [][]int) string {
-	cities := make([]int, 0)
-	outStr := make([]string, 0)
-	for _, path := range paths {
-		for _, city := range path {
-			if !isInList(cities, city) {
-				cities = append(cities, city)
-			}
-		}
-	}
 
-	sort.Ints(cities)
-
-	for _, city := range cities {
-		outStr = append(outStr, strconv.Itoa(city))
-	}
-
-	return strings.Join(outStr, " ")
-}
-
-func isInList(list []int, elem int) bool {
-	for _, v := range list {
-		if v == elem {
-			return true
-		}
-	}
-	return false
-}
-
-func Dijsktra(graph Graph, start int) (map[int][]int, map[int]int) {
-	visited := make([]int, 0)
+func dijsktra(graph Graph, start int) (map[int][]int, map[int]int) {
+	visited := make([]bool, graph.GetNumOfNodes()+1)
 	distance := make(map[int]int)
-	restNode := make([]int, 0)
 	path := make(map[int][]int)
+	restNode := make([]int, graph.GetNumOfNodes()+1)
 
-	for _, node := range *(graph.GetNodes()) {
-		distance[node] = math.MaxInt16
-		restNode = append(restNode, node)
-		path[node] = []int{}
+	for i, v := range *graph.GetNodes() {
+		if v {
+			distance[i] = math.MaxInt16
+			restNode[i] = 1
+			visited[i] = false
+			path[i] = []int{}
+		}
 	}
 
 	distance[start] = 0
 
-	for len(restNode) != 0 {
-		minNode := -9999
+	numOfRestNode := graph.GetNumOfNodes()
+
+	for numOfRestNode != 0 {
+		minNode := math.MinInt16
 		minDistance := math.MaxInt16
 
 		for node := range distance {
-			if distance[node] < minDistance && !isContain(node, &visited) {
+			if distance[node] < minDistance && !visited[node] {
 				minDistance = distance[node]
 				minNode = node
 			}
 		}
 
-		restNode = *Remove(minNode, &restNode)
+		if minNode < 0 {
+			break
+		}
+		if restNode[minNode] != 0 {
+			restNode[minNode] = 0
+			numOfRestNode -= 1
+		}
 
 		for _, node := range ((*graph.GetEdges())[minNode]) {
-			newDistance := distance[minNode] + (*graph.GetDistances())[Edge{minNode, node}]
+			newDistance := distance[minNode] + 1
 			if newDistance < distance[node] {
 				path[node] = []int{}
 				distance[node] = newDistance
@@ -129,49 +112,11 @@ func Dijsktra(graph Graph, start int) (map[int][]int, map[int]int) {
 			}
 		}
 
-		visited = append(visited, minNode)
+		visited[minNode] = true
 	}
 
 	return path, distance
 }
-func Remove(element int, slice *[]int) *[]int{
-	temp := make([]int, len(*slice))
-	copy(temp, *slice)
-	for i, e := range *slice {
-		if e == element {
-			result := append(temp[:i], temp[i+1:]...)
-			return &result
-		}
-	}
-	return slice
-}
-
-func isContain(element int, slice *[]int) bool {
-	for _, e := range *slice {
-		if e == element {
-			return true
-		}
-	}
-	return false
-}
-
-func GetMultiPath(path map[int][]int, start int, end int, past []int, multiPath *[][]int) {
-	if start != end {
-		past = append(past, end)
-	} else {
-		past = append(past, start)
-	}
-
-	for _, node := range path[end] {
-		GetMultiPath(path, start, node, past, multiPath)
-	}
-
-	if past[len(past) - 1] == start {
-		*multiPath = append(*multiPath, past)
-	}
-	past = past[:len(past)-1]
-}
-
 
 type Edge struct {
 	start int
@@ -179,36 +124,30 @@ type Edge struct {
 }
 
 type Graph struct {
-	nodes []int
+	nodes []bool
 	edges map[int][]int
-	distances map[Edge]int
 }
 
-func InitGraph() Graph {
+func InitGraph(n int) Graph {
 	graph := Graph{
-		make([]int, 0),
+		make([]bool, n + 1),
 		make(map[int][]int),
-		make(map[Edge]int),
 	}
+
 	return graph
 }
 
-func (g *Graph) AddNode(node int) {
-	for _, n := range g.nodes {
-		if n == node {
-			return
-		}
-	}
-	g.nodes = append(g.nodes, node)
-	g.edges[node] = []int{}
-}
-
 func (g *Graph) AddEdge(start int, to int, distance int) {
+	g.nodes[start] = true
+	g.nodes[to] = true
 	g.edges[start] = append(g.edges[start], to)
-	g.distances[Edge{start, to}] = distance
 }
 
-func (g *Graph) GetNodes() *([]int) {
+func (g *Graph) GetNumOfNodes() int {
+	return len(g.nodes) - 1
+}
+
+func (g *Graph) GetNodes() *([]bool) {
 	return &g.nodes
 }
 
@@ -216,25 +155,12 @@ func (g *Graph) GetEdges() *(map[int][]int) {
 	return &g.edges
 }
 
-func (g *Graph) GetDistances() *(map[Edge]int) {
-	return &g.distances
-}
-
-func DFS(path *map[int][]int, v int, discovered *[]int) {
-	*discovered = append(*discovered, v)
+func DFS(path *map[int][]int, v int, discovered *[]bool) {
+	(*discovered)[v] = true
 
 	for _, node := range (*path)[v] {
-		if isInSlice(discovered, node) == false {
+		if (*discovered)[node] == false {
 			DFS(path, node, discovered)
 		}
 	}
-}
-
-func isInSlice(slice *[]int, elem int) bool {
-	for _, v := range *slice {
-		if elem == v {
-			return true
-		}
-	}
-	return false
 }
