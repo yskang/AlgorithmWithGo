5525415a64bbdd9eeecc476f7cdb354713536ef2
Yongsung Kang <yongsung.kang@gmail.com>
2016-10-26 14:48:56 +0900

    time limit..

--- inSolving/skiCourse.go
+++ inSolving/skiCourse.go
@@ -4,43 +4,52 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"bufio"
 	"os"
 	"strings"
 	"strconv"
+	"math"
 )
 
-//var count, cacheCount int
-
 func main() {
+
+
 	var T int
 	fmt.Scanf("%d\n", &T)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for t := 0 ; t < T ; t++ {
+
 		var inStr string
 		for scanner.Scan() {
 			inStr = scanner.Text()
 			break
 		}
 		inLine := strings.Split(inStr, " ")
-		N, _ := strconv.Atoi(inLine[0])
-		M, _ := strconv.Atoi(inLine[1])
-		S, _ := strconv.Atoi(inLine[2])
+		var N uint16
+		var M uint32
+		var S uint8
+		var temp int
 
-		graph := make([][]int, 0)
-		for i := 0 ; i <= N ; i++ {
-			graph = append(graph, make([]int, N+1))
+		temp, _ = strconv.Atoi(inLine[0])
+		N = uint16(temp)
+		temp, _ = strconv.Atoi(inLine[1])
+		M = uint32(temp)
+		temp, _ = strconv.Atoi(inLine[2])
+		S = uint8(temp)
+
+		graph := make([]map[uint16]int32, 0)
+		for i := uint16(0) ; i < N ; i++ {
+			graph = append(graph, make(map[uint16]int32))
 		}
 
-		cache := make([][]int, 0)
-		for i := 0 ; i <= N ; i++ {
-			cache = append(cache, make([]int, S+1))
+		cache := make([][]int32, 0)
+		for i := uint16(0) ; i < N ; i++ {
+			cache = append(cache, make([]int32, S))
 		}
 
-		for m := 0 ; m < M ; m++ {
+		for m := uint32(0) ; m < M ; m++ {
 			var inStr2 string
 			for scanner.Scan() {
 				inStr2 = scanner.Text()
@@ -50,84 +59,53 @@ func main() {
 			A, _ := strconv.Atoi(inLine2[0])
 			B, _ := strconv.Atoi(inLine2[1])
 			C, _ := strconv.Atoi(inLine2[2])
-
-			graph[A-1][B-1] = C
+			graph[B-1][uint16(A-1)] = int32(C)
 		}
 
-		maxExcitingValue := math.MinInt16
-		for node := 1 ; node <= N ; node++ {
-			var excitingValue int
+		maxExcitingValue := int32(math.MinInt32)
+		var excitingValue int32
 
-			excitingValue = getMaxExcitingValueAt(&cache, &graph, node, S)
-			cache[node][S] = excitingValue
+		for slope := uint8(1) ; slope <= S ; slope++ {
+			for node := uint16(0) ; node < N ; node++ {
 
+				excitingValue = getMaxExcitingValueAt(&cache, &graph, node, slope)
 
-			if maxExcitingValue < excitingValue {
-				maxExcitingValue = excitingValue
+				cache[node][slope-1] = excitingValue
+
+				if maxExcitingValue < excitingValue {
+					maxExcitingValue = excitingValue
+				}
+
 			}
-
 		}
-		//fmt.Println("call count", count)
 
-		//fmt.Println("cache hit count",cacheCount)
-		//fmt.Println(cache)
-		//count = 0
-		//cacheCount = 0
 		fmt.Println(maxExcitingValue)
 	}
-
 }
 
-func getMaxExcitingValueAt(cache *[][]int, graph *[][]int, node int, depth int) int {
-	if (*cache)[node][depth] != 0 {
-		//cacheCount += 1
-		return (*cache)[node][depth]
-	}
+func getMaxExcitingValueAt(cache *[][]int32, graph *[]map[uint16]int32, node uint16, depth uint8) int32 {
+	maxExcitingValue := int32(math.MinInt32)
+	excitingValue := int32(0)
 
-	//count += 1
-
-	if depth == 1 {
-		maxExcitingValue := math.MinInt16
-
-		for prevNode := 0 ; prevNode < len(*graph) ; prevNode++ {
-			if maxExcitingValue < (*graph)[prevNode][node] {
-				maxExcitingValue = (*graph)[prevNode][node]
-			}
-		}
-
-		(*cache)[node][depth] = maxExcitingValue
-		return maxExcitingValue
-	}
-
-	maxExcitingValue := math.MinInt16
-	maxNode := 0
-
-	for prevNode := 0 ; prevNode < len(*graph) ; prevNode++ {
-		if (*graph)[prevNode][node] != 0 {
-			var excitingValue int
-			candidates := make([]int, 0)
-			excitingValue = getMaxExcitingValueAt(cache, graph, prevNode, depth -1)
-			if excitingValue > maxExcitingValue {
-				maxExcitingValue = excitingValue
-				maxNode = prevNode
-				candidates = candidates[:0]
-			} else if excitingValue == maxExcitingValue {
-				if (*graph)[prevNode][node] > (*graph)[maxNode][node] {
-					maxNode = prevNode
-				}
-			}
-		}
-	}
-
-	if maxExcitingValue == math.MinInt16 && maxNode == 0 {
+	if len((*graph)[node]) == 0 {
 		return 0
 	}
 
-	excitingValue := maxExcitingValue + (*graph)[maxNode][node]
-	if excitingValue <= 0 {
-		return 0
+	for prevNode := range (*graph)[node] {
+		if depth == 1 {
+			excitingValue = (*graph)[node][prevNode]
+		} else {
+			if (*cache)[prevNode][depth-2] != 0 {
+				excitingValue = (*cache)[prevNode][depth-2] + (*graph)[node][prevNode]
+			} else {
+				excitingValue = getMaxExcitingValueAt(cache, graph, prevNode, depth-1) + (*graph)[node][prevNode]
+			}
+		}
+		if maxExcitingValue < excitingValue {
+			maxExcitingValue = excitingValue
+		}
 	}
 
-	(*cache)[node][depth] = excitingValue
-	return excitingValue
+	(*cache)[node][depth-1] = maxExcitingValue
+	return maxExcitingValue
 }
