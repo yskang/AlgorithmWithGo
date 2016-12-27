dcb141d70c6ae672bb862911691586cf9566f812
Yongsung Kang <yongsung.kang@gmail.com>
2016-12-27 23:29:04 +0900

    add hamming distance problem for leetcode

---
+++ leetcode/hammingDistance.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(hammingDistance(1, 15))
+}
+
+func hammingDistance(x int, y int) int {
+	z := x ^ y
+	result := 0
+
+	for i := 0 ; i < 32 ; i++{
+		result += z & 1
+		z >>= 1
+	}
+
+	return result
+}
+
