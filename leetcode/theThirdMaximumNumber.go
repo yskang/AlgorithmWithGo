30ab09b73ae611ebf19aaf046714762d146001b9
Yongsung Kang <yongsung.kang@lge.com>
2017-02-24 14:07:12 +0900

    add my solution of leetcode problem, get the third maximum number

---
+++ leetcode/theThirdMaximumNumber.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(thirdMax([]int{2,2,3,1}))
+}
+
+func thirdMax(nums []int) int {
+	sort.Ints(nums)
+	before := nums[len(nums) - 1]
+	count := 1
+	for i := len(nums) - 1 ; i >= 0 ; i-- {
+		if before != nums[i] {
+			count += 1
+			before = nums[i]
+			if count == 3 {
+				return before
+			}
+		}
+	}
+	return nums[len(nums)-1]
+}
