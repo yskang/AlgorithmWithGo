c16c21e24b1a2ae7afc2347a1262e6edaa1a3f23
Yongsung Kang <yongsung.kang@lge.com>
2017-02-27 16:11:13 +0900

    add my solution for leetcode problem, valid palindrome

---
+++ leetcode/validPalindrome.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(isPalindrome("race a car"))
+}
+
+func isPalindrome(s string) bool {
+	sInLower := strings.ToLower(s)
+	onlyAlphabet := ""
+	reverseAlphabet := ""
+
+	for _, letter := range(sInLower) {
+		if (letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9') {
+			onlyAlphabet += string(letter)
+			reverseAlphabet = string(letter) + reverseAlphabet
+		}
+	}
+
+	if onlyAlphabet == reverseAlphabet {
+		return true
+	}
+	return false
+}
