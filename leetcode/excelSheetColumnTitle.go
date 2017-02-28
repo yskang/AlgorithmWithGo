b07f5a1768238c9cc956a68641edceda3e260982
Yongsung Kang <yongsung.kang@lge.com>
2017-02-28 11:25:20 +0900

    add my solution for leetcode problem, excel title

---
+++ leetcode/excelSheetColumnTitle.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(convertToTitle(1))
+	fmt.Println(convertToTitle(26))
+	fmt.Println(convertToTitle(27))
+	fmt.Println(convertToTitle(51))
+	fmt.Println(convertToTitle(52))
+	fmt.Println(convertToTitle(53))
+	fmt.Println(convertToTitle(54))
+	fmt.Println(convertToTitle(55))
+	fmt.Println(convertToTitle(78))
+	fmt.Println(convertToTitle(79))
+}
+
+func convertToTitle(n int) string {
+	title := ""
+	for ; n != 0;{
+		char := 'A' + (n - 1) % 26
+		n = (n - 1) / 26
+		title = string(char) + title
+	}
+
+	return title
+}
