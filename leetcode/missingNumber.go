88706da55360c4c0bd13b0c5a0d7525ba774255f
Yongsung Kang <yongsung.kang@lge.com>
2017-03-03 16:58:16 +0900

    add my solution for leetcode problem, finding missing number

---
+++ leetcode/missingNumber.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(missingNumber([]int{0,1,2,3,4,6,7,8,9}))
+}
+
+func missingNumber(nums []int) int {
+	sort.Ints(nums)
+
+	for index, num := range(nums) {
+		if index != num {
+			return index
+		}
+	}
+
+	return len(nums)
+}
