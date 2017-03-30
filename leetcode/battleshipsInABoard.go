a27a370d0e10b2e48fb10dbeafc058c355dca7f5
Yongsung Kang <yongsung.kang@lge.com>
2017-03-30 14:04:51 +0900

    add my solution for leetcode problem, count battleships in a board

---
+++ leetcode/battleshipsInABoard.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(countBattleships([][]byte{{'X','.','.','X'},
+		{'.','.','.','X'},
+		{'.','.','.','X'}}))
+	fmt.Println(countBattleships([][]byte{  {'X','.','.','X'},
+						{'.','X','.','X'},
+						{'.','X','.','.'},
+						{'.','.','.','X'},
+						{'X','X','.','X'}}))
+}
+
+func countBattleships(board [][]byte) int {
+	count := 0
+
+	for y := 0 ; y < len(board) ; y++ {
+		for x := 0 ; x < len(board[0]) ; x++ {
+			fmt.Print(string(board[y][x]))
+			count += checkLeftAndUp(x, y, board)
+		}
+		fmt.Println();
+	}
+
+	return count
+}
+func checkLeftAndUp(x int, y int, board [][]byte) int {
+	hasLeft := false
+	hasUp := false
+
+	if x != 0 && board[y][x-1] == 'X' {
+		hasLeft = true
+	}
+	if y != 0 && board[y-1][x] == 'X' {
+		hasUp = true
+	}
+
+	if board[y][x] == 'X' && hasLeft == false && hasUp == false {
+		return 1
+	}
+	return 0
+}
