e4ba21d7b6a21b1069688070b0566574be6c85b8
Yongsung Kang <yongsung.kang@lge.com>
2017-03-30 11:46:40 +0900

    my solution of leetcode problem, complex number multiplication

---
+++ leetcode/complexNumberMultiplication.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf(complexNumberMultiply("1+1i", "1+1i"))
+}
+
+func complexNumberMultiply(a string, b string) string {
+	as := strings.Split(a, "+")
+	bs := strings.Split(b, "+")
+
+	aReal, _ := strconv.Atoi(as[0])
+	aImage, _ := strconv.Atoi(strings.Replace(as[1], "i", "", 1))
+	bReal, _ := strconv.Atoi(bs[0])
+	bImage, _ := strconv.Atoi(strings.Replace(bs[1], "i", "", 1))
+
+	real := strconv.Itoa(aReal * bReal - aImage * bImage)
+	image := strconv.Itoa(aReal * bImage + aImage * bReal)
+
+	return real + "+" + image + "i"
+}
