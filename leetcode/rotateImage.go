db64c0a753fd95a12a5ae91a81badd163efc214b
Yongsung Kang <yongsung.kang@gmail.com>
2017-04-25 00:16:20 +0900

    remove a temp value

--- leetcode/rotateImage.go
+++ leetcode/rotateImage.go
@@ -13,9 +13,7 @@ func rotateImage(matrix [][]int) {
 
 	for i := 0 ; i < len(matrix) ; i++ {
 		for j := i + 1 ; j < len(matrix[i]) ; j++ {
-			temp := matrix[i][j]
-			matrix[i][j] = matrix[j][i]
-			matrix[j][i] = temp
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 		}
 	}
 }
