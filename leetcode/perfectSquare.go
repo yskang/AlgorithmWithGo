30ce90469126e800a1cdacf351d04858858b6b1f
Yongsung Kang <yongsung.kang@gmail.com>
2017-04-24 23:42:43 +0900

    Perfect Squares, leetcode

--- leetcode/perfectSquare.go
+++ leetcode/perfectSquare.go
@@ -1,1 +1,25 @@
 package leetcode
+
+func PerfectSquare(n int) int {
+	return numSquares(n)
+}
+
+func numSquares(n int) int {
+	nums := []int{n}
+	nexts := make([]int, 0)
+
+	for i := 1 ; true ; i++ {
+		for _, num := range nums {
+			for j := 1 ; num - (j*j) >= 0 ; j++ {
+				if num - (j*j) == 0 {
+					return i
+				}
+				nexts = append(nexts, num - (j*j))
+			}
+		}
+		nums = make([]int, len(nexts))
+		copy(nums, nexts)
+		nexts = make([]int, 0)
+	}
+	return n
+}
