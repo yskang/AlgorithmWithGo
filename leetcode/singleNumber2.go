42870fc45c9941a372c62424c768fc76c4f9f2f9
Yongsung Kang <yongsung@Yongsungs-Mac-Pro.local>
2017-07-20 15:32:20 +0900

    single number 2, leetcode

---
+++ leetcode/singleNumber2.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func SingleNumber2(nums []int) []int {
+	return singleNumber(nums)
+}
+
+func singleNumber(nums []int) []int {
+	twoNums := 0
+	for _, num := range nums {
+		twoNums ^= num
+	}
+
+	bitIndex := 0
+	for {
+		if twoNums & 0x01 == 1 {
+			break
+		}
+		twoNums >>= 1
+		bitIndex += 1
+	}
+
+	firstNum := 0
+	secondNum := 0
+	for _, num := range nums {
+		if (num >> uint(bitIndex)) & 0x01 == 1 {
+			firstNum ^= num
+		} else {
+			secondNum ^= num
+		}
+	}
+
+	return []int{firstNum, secondNum}
+}
---
+++ tests/singleNumber2_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"AlgorithmWithGo/leetcode"
+	"fmt"
+)
+
+func ExampleSingleNumber2() {
+	fmt.Println(leetcode.SingleNumber2([]int{1,2,1,3,2,5}))
+	// output:
+	// [3 5]
+}
