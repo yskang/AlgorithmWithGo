56632e891870daa9e0fc48fd3e88b09d03aa191d
Yongsung Kang <yongsung.kang@gmail.com>
2017-08-14 15:04:30 +0900

    Total Hamming distance, leetcode

--- leetcode/totalHammingDistance.go
+++ leetcode/totalHammingDistance.go
@@ -5,25 +5,19 @@ func TotalHammingDistance(nums []int) int {
 }
 
 func totalHammingDistance(nums []int) int {
-	sum := uint32(0)
+	sum := 0
+	shiftCount := uint(0)
 
-	for index := 0 ; index < len(nums) ; index++ {
-		for i := index + 1 ; i < len(nums) ; i++ {
-			sum += getHammingDistance(nums[index], nums[i])
+	for shiftCount < 32 {
+		zeros := 0
+		for _, num := range nums {
+			num = num >> shiftCount
+			if num & 0x01 == 0 {
+				zeros += 1
+			}
 		}
-	}
-
-	return int(sum)
-}
-
-func getHammingDistance(f int, s int) uint32 {
-	a := uint32(f)
-	b := uint32(s)
-	xor := a ^ b
-	sum := uint32(0)
-
-	for i := 0 ; i < 32 ; i++ {
-		sum += (xor >> uint32(i)) & 1
+		sum += zeros * (len(nums) - zeros)
+		shiftCount += 1
 	}
 
 	return sum
