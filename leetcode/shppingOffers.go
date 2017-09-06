c9d41908763d10758041d955ba471ce71ae1409d
Yongsung Kang <yongsung.kang@gmail.com>
2017-09-06 16:47:07 +0900

    add templete

---
+++ leetcode/shppingOffers.go
@@ -0,0 +1,9 @@
+package leetcode
+
+func ShoppingOffers(price []int, special [][]int, needs []int) int {
+	return shoppingOffers(price, special, needs)
+}
+
+func shoppingOffers(price []int, special [][]int, needs []int) int {
+	return 0
+}
