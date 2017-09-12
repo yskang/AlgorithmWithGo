ae803be8f4c2fd3b08ce1a6a66a07b43a3a14f7b
Yongsung Kang <yongsung.kang@gmail.com>
2017-09-12 22:35:54 +0900

    guess number higher or lower, leetcode

--- leetcode/guessNumberHigherOrLowerII.go
+++ leetcode/guessNumberHigherOrLowerII.go
@@ -1,31 +1,41 @@
 package leetcode
 
+import (
+	"math"
+)
+
 func GetMoneyAmount(n int) int {
 	return getMoneyAmount(n)
 }
 
 func getMoneyAmount(n int) int {
-
-	dp := make([][]int, n)
+	dp := make([][]int, n+1)
 	for i := range dp {
-		dp[i] = make([]int, n)
+		dp[i] = make([]int, n+1)
 	}
 
-	for l := 0 ; l < n ; l++ {
-		for s := 0 ; s+l < n ; s++ {
+	for l := 1 ; l <= n ; l++ {
+		for s := 1 ; s+l <= n ; s++ {
 			if l == 0 {
 				dp[s][s+l] = 0
 			} else if l == 1 {
-				dp[s][s+1] = s
+				dp[s][s+l] = s
 			} else {
-				for k := s + 1 ; k < s+l; k++ {
-					dp[s][s+l] = minMon(dp[s][s+l], k + maxMon(dp[s][k-1], dp[k+1][s+l]))
+				dp[s][s+l] = math.MaxInt16
+				for k := s ; k <= s+l ; k++ {
+					if s == k {
+						dp[s][s+l] = minMon(dp[s][s+l], k + maxMon(0, dp[k+1][s+l]))
+					} else if k == s+l {
+						dp[s][s+l] = minMon(dp[s][s+l], k + maxMon(dp[s][k-1], 0))
+					} else {
+						dp[s][s+l] = minMon(dp[s][s+l], k + maxMon(dp[s][k-1], dp[k+1][s+l]))
+					}
 				}
 			}
 		}
 	}
 
-	return dp[0][n-1]
+	return dp[1][n]
 }
 
 func minMon(a, b int) int {
