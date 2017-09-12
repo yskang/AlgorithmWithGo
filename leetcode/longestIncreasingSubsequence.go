eefa7e221c0e50c9a20a924f0c81e2187532759b
Yongsung Kang <yongsung.kang@gmail.com>
2017-09-12 15:18:09 +0900

    longest increasing subsequence, leetcode

--- leetcode/longestIncreasingSubsequence.go
+++ leetcode/longestIncreasingSubsequence.go
@@ -1,24 +1,57 @@
 package leetcode
 
-import "fmt"
-
 func LengthOfLIS(nums []int) int {
 	return lengthOfLIS(nums)
 }
 
 func lengthOfLIS(nums []int) int {
-	fmt.Println(nums)
+	if len(nums) < 2 {
+		return len(nums)
+	}
+	//fmt.Println(nums)
+	dp := make([]int, len(nums))
+	dp[0] = 1
+	maxLIS := -1
+	for i := 1 ; i < len(nums) ; i++ {
+		maxDp := -1
+		for j := i-1 ; j >= 0 ; j-- {
+			if nums[j] < nums[i] {
+				if maxDp < dp[j] {
+					maxDp = dp[j]
+				}
+			}
+		}
+		if maxDp == -1 {
+			dp[i] = 1
+		} else {
+			dp[i] = maxDp + 1
+		}
+
+		if dp[i] > maxLIS {
+			maxLIS = dp[i]
+		}
+		//	fmt.Println(dp)
+	}
+	return maxLIS
+}
+
+// solution of Memory Limit Exceeded
+func lengthOfLIS_MLE(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
 	dp := make([][]minMax, len(nums))
 	for i := range nums {
 		dp[i] = make([]minMax, len(nums))
 	}
 
-	for s := 0 ; s < len(nums) ; s++ {
-		for l := 0 ; s+l < len(nums) ; l++ {
+	for l := 0 ; l < len(nums) ; l++ {
+		for s := 0 ; s+l < len(nums) ; s++ {
 			dp[s][s+l] = getLIS(s, s+l, nums, dp)
 		}
 	}
-	fmt.Println(dp)
+
 	return dp[0][len(nums)-1].length
 }
 
@@ -32,30 +65,16 @@ func getLIS(s int, e int, nums []int, dp [][]minMax) minMax {
 		return minMax{nums[e], nums[s], 1}
 	}
 
-	if dp[s+1][e].min > nums[s] && dp[s][e-1].max < nums[e] {
-		return minMax{nums[s], nums[e], dp[s+1][e-1].length+2}
-	} else if dp[s+1][e].min > nums[s] {
+	if dp[s+1][e].min > nums[s] {
 		return minMax{nums[s], dp[s+1][e].max, dp[s+1][e].length+1}
 	} else if dp[s][e-1].max < nums[e] {
 		return minMax{dp[s][e-1].min, nums[e], dp[s][e-1].length+1}
 	}
 
-	mininum := 0
-	maximum := 0
-
-	if nums[s] > dp[s+1][e].max {
-		maximum = nums[s]
-	} else {
-		maximum = dp[s+1][e].max
+	if dp[s+1][e].length > dp[s][e-1].length {
+		return dp[s+1][e]
 	}
-
-	if nums[e] < dp[s][e-1].min {
-		mininum = nums[e]
-	} else {
-		mininum = dp[s][e-1].min
-	}
-
-	return minMax{mininum, maximum, dp[s+1][e-1].length}
+	return dp[s][e-1]
 }
 
 type minMax struct {
