33cd6fc9d961da69ac09ca5a377ed9a859afe00a
Yongsung Kang <yongsung.kang@gmail.com>
2017-12-12 16:13:58 +0900

    House Robber III, leetcode

--- leetcode/houseRobberIII.go
+++ leetcode/houseRobberIII.go
@@ -6,15 +6,39 @@ func RobIII(root *myLibs.TreeNode) int {
 	return robIII(root)
 }
 
-func robIII_(root *myLibs.TreeNode) int {
-	return robWithOrWithout(root)
+func robIII(root *myLibs.TreeNode) int {
+	withRoot, withoutRoot := depthFirst(root)
+	return getMaxRob(withRoot, withoutRoot)
+}
+
+func depthFirst(r *myLibs.TreeNode) (int, int) {
+	if r == nil {
+		return 0, 0
+	}
+	leftWithRoot, leftWithoutRoot := depthFirst(r.Left)
+	rightWithRoot, rightWithoutRoot := depthFirst(r.Right)
+	withRoot := leftWithoutRoot + rightWithoutRoot + r.Val
+	withoutRoot := getMaxRob(leftWithoutRoot, leftWithRoot) + getMaxRob(rightWithRoot, rightWithoutRoot)
+	return withRoot, withoutRoot
+}
+
+
+func robIII__(root *myLibs.TreeNode) int {
+	return getMaxRob(robWithoutRoot(root), robWithRoot(root))
 }
 
 func robWithoutRoot(r *myLibs.TreeNode) int {
 	if r == nil {
 		return 0
 	}
-	return robWithOrWithout(r.Left) + robWithOrWithout(r.Right)
+	return robIII(r.Left) + robIII(r.Right)
+}
+
+func robWithRoot(r *myLibs.TreeNode) int {
+	if r == nil {
+		return 0
+	}
+	return r.Val + robWithoutRoot(r.Left) + robWithoutRoot(r.Right)
 }
 
 func robWithOrWithout(r *myLibs.TreeNode) int {
@@ -24,7 +48,7 @@ func robWithOrWithout(r *myLibs.TreeNode) int {
 	return getMaxRob(robWithoutRoot(r.Left) + robWithoutRoot(r.Right) + r.Val, robWithoutRoot(r))
 }
 
-func robIII(root *myLibs.TreeNode) int {
+func robIII_(root *myLibs.TreeNode) int {
 	robWithoutRoot := func(r *myLibs.TreeNode) int {
 		if r == nil {
 			return 0
