b890507a377a1ab8006db23c40f222b7abffabec
Yongsung Kang <yongsung.kang@gmail.com>
2017-12-18 16:47:47 +0900

    Shortest Completing Word, leetcode

--- leetcode/leetcodeTest/shortestCompletingWord_test.go
+++ leetcode/leetcodeTest/shortestCompletingWord_test.go
@@ -1,1 +1,14 @@
 package leetcodeTest
+
+import (
+	"fmt"
+	"AlgorithmWithGo/leetcode"
+)
+
+func ExampleShortestCompletingWord() {
+	fmt.Println(leetcode.ShortestCompletingWord("1s3 PSt", []string{"step", "steps", "stripe", "stepple"}))
+	fmt.Println(leetcode.ShortestCompletingWord("1s3 456", []string{"looks", "pest", "stew", "show"}))
+	// output:
+	// steps
+	// pest
+}
--- leetcode/shortestCompletingWord.go
+++ leetcode/shortestCompletingWord.go
@@ -1,1 +1,53 @@
 package leetcode
+
+import (
+	"strings"
+	"math"
+)
+
+func ShortestCompletingWord(licensePlate string, words []string) string {
+	return shortestCompletingWord(licensePlate, words)
+}
+
+func shortestCompletingWord(licensePlate string, words []string) string {
+	dic := make(map[rune]int)
+
+	for _, c := range strings.ToLower(licensePlate) {
+		if c >= 'a' && c <= 'z' {
+			dic[c] += 1
+		}
+	}
+
+	cans := make([]string, 0)
+	shortestLength := math.MaxInt64
+
+	for _, str := range words {
+		if checkStr(str, dic) {
+			cans = append(cans, str)
+			if len(str) < shortestLength {
+				shortestLength = len(str)
+			}
+		}
+	}
+
+	for _, str := range cans {
+		if len(str) == shortestLength {
+			return str
+		}
+	}
+
+	return ""
+
+}
+
+func checkStr(s string, dic map[rune]int) bool {
+	for char, count := range dic {
+		 if strings.Count(strings.ToLower(s), string(char)) >= count {
+		 	continue
+		 } else {
+		 	return false
+		 }
+	}
+	return true
+}
+
