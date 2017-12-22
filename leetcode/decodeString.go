ae509a2831dcedf6d79a87ad71ae039c5cb197cf
Yongsung Kang <yongsung.kang@gmail.com>
2017-12-22 13:14:02 +0900

    Decode String, leetcode

--- leetcode/decodeString.go
+++ leetcode/decodeString.go
@@ -1,7 +1,6 @@
 package leetcode
 
 import (
-	"expvar"
 	"strconv"
 	"strings"
 )
@@ -13,47 +12,90 @@ func DecodeString(s string) string {
 const (
 	STR = iota
 	NUM = iota
-	PAL = iota
-	NON = iota
+	BR  = iota
 )
 
 func decodeString(s string) string {
-	stack := newStringStack()
 	ans := make([]string, 0)
+	stack := newStringStack()
 
 	for _, c := range s {
-		if !stack.isEmpty() {
-			prev := stack.pop()
-			strings.
-		} else {
+		if stack.isEmpty() {
 			stack.push(string(c))
+		} else {
+			if getKind(c) == BR {
+				if c == '[' {
+					stack.push(string(c))
+				} else {
+					// fmt.Println(stack.String())
+					temp := stack.pop()
+					stack.pop()
+					num, _ := strconv.Atoi(stack.pop())
+					if stack.isEmpty() {
+						ans = append(ans, strings.Repeat(temp, num))
+					} else {
+						tail := strings.Repeat(temp, num)
+						if getKind(rune(stack.checkTop()[0])) == STR {
+							head := stack.pop()
+							stack.push(head + tail)
+						} else {
+							stack.push(tail)
+						}
+					}
+				}
+			} else if getKind(rune(stack.checkTop()[0])) == getKind(c) {
+				stack.push(stack.pop() + string(c))
+			} else {
+				stack.push(string(c))
+			}
 		}
 	}
 
+	// fmt.Println(stack.String())
+	if !stack.isEmpty() {
+		return strings.Join(ans, "") + stack.pop()
+	}
+	return strings.Join(ans, "")
+}
+
+func getKind(c rune) int {
+	if '0' <= c && c <= '9' {
+		return NUM
+	} else if 'a' <= c && c <= 'z' {
+		return STR
+	}
+	return BR
+}
+
+type StringStack struct {
+	strs []string
+}
+
+func newStringStack() StringStack {
+	return StringStack{make([]string, 0)}
+}
+
+func (s *StringStack) push(str string) {
+	s.strs = append(s.strs, str)
+}
+
+func (s *StringStack) pop() string {
+	ans := s.strs[len(s.strs)-1]
+	s.strs = append([]string{}, s.strs[:len(s.strs)-1]...)
 	return ans
 }
 
-type StringStask struct {
-	strings []string
+func (s *StringStack) checkTop() string {
+	return s.strs[len(s.strs)-1]
 }
 
-func newStringStack() StringStask {
-	return StringStask{make([]string, 0)}
-}
-
-func (s *StringStask) push(str string) {
-	s.strings = append(s.strings, str)
-}
-
-func (s *StringStask) pop() string {
-	ans := s.strings[len(s.strings)-1]
-	s.strings = append([]string{}, s.strings[:len(s.strings)-1]...)
-	return ans
-}
-
-func (s *StringStask) isEmpty() bool {
-	if len(s.strings) == 0 {
+func (s *StringStack) isEmpty() bool {
+	if len(s.strs) == 0 {
 		return true
 	}
 	return false
+}
+
+func (s *StringStack) String() string {
+	return "B {" + strings.Join(s.strs, " | ") + "} T"
 }
--- leetcode/leetcodeTest/decodeString_test.go
+++ leetcode/leetcodeTest/decodeString_test.go
@@ -1,16 +1,18 @@
 package leetcodeTest
 
 import (
-	"fmt"
 	"AlgorithmWithGo/leetcode"
+	"fmt"
 )
 
 func ExampleDecodeString() {
 	fmt.Println(leetcode.DecodeString("3[a]2[bc]"))
 	fmt.Println(leetcode.DecodeString("3[a2[c]]"))
 	fmt.Println(leetcode.DecodeString("2[abc]3[cd]ef"))
+	fmt.Println(leetcode.DecodeString("2[2[b]]"))
 	// output:
 	// aaabcbc
 	// accaccacc
 	// abcabccdcdcdef
+	// bbbb
 }
