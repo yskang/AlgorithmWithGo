c2c166f3d13f50daaed1805437e346d5c4af8621
Yongsung Kang <yongsung.kang@gmail.com>
2017-12-28 19:27:44 +0900

    create file

---
+++ leetcode/containerWithMostWater.go
@@ -0,0 +1,9 @@
+package leetcode
+
+func MaxArea(height []int) int {
+	return maxArea(height)
+}
+
+func maxArea(height []int) int {
+	return 0
+}
---
+++ leetcode/leetcodeTest/containerWithMostWater_test.go
@@ -0,0 +1,12 @@
+package leetcodeTest
+
+import (
+	"AlgorithmWithGo/leetcode"
+	"fmt"
+)
+
+func ExampleContainerWithMostWater() {
+	fmt.Println(leetcode.MaxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	// output:
+	// 49
+}
