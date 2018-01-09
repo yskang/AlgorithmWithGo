24a4a2c3a1fda04beedbcac0e715376f1fbd390f
Yongsung Kang <yongsung.kang@gmail.com>
2018-01-09 18:29:48 +0900

    Word Break, add dp solution

--- leetcode/wordBreak.go
+++ leetcode/wordBreak.go
@@ -5,7 +5,29 @@ func WordBreak(s string, wordDict []string) bool {
 	return wordBreak(s, wordDict)
 }
 
+// use dp
 func wordBreak(s string, wordDict []string) bool {
+	dict := make(map[string]bool)
+	f := make([]bool, len(s)+1)
+	f[0] = true
+
+	for _, word := range wordDict {
+		dict[word] = true
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if f[j] && dict[s[j:i]] {
+				f[i] = true
+				break
+			}
+		}
+	}
+	return f[len(s)]
+}
+
+// use dfs
+func wordBreakDFS(s string, wordDict []string) bool {
 	ans := false
 	wordCheck(s, wordDict, &ans)
 	return ans
