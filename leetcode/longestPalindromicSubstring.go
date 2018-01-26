23aae239194866c1155f15c40be013a86feee722
Yongsung Kang <yongsung.kang@gmail.com>
2018-01-26 15:22:13 +0900

    Longest Palindromic Substring (longest substring method), leetcode

--- leetcode/longestPalindromicSubstring.go
+++ leetcode/longestPalindromicSubstring.go
@@ -1,10 +1,40 @@
 package leetcode
 
+import (
+	"fmt"
+)
+
 func LongestPalindrome(s string) string {
 	return longestPalindrome(s)
 }
 
 func longestPalindrome(s string) string {
+	dp := make([][]int, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(s)+1)
+	}
+
+	maxStr := ""
+
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			if s[i] == s[len(s)-1-j] {
+				dp[i+1][j+1] = dp[i][j] + 1
+				if dp[i+1][j+1] > len(maxStr) && isPal(s[i+1-dp[i+1][j+1]:i+1]) {
+					maxStr = s[i+1-dp[i+1][j+1] : i+1]
+				}
+			}
+		}
+	}
+
+	for _, row := range dp {
+		fmt.Println(row)
+	}
+
+	return maxStr
+}
+
+func longestPalindromeBruteForce(s string) string {
 	lgstPal := ""
 
 	for i := 0; i < len(s); i++ {
